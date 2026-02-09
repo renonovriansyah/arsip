e4f43cf8ed501672b496f6e44d0ea765163ec366
renonovriansyah <renonovriansyah9@gmail.com>
2026-02-09 11:32:50 +0700

    Siap Hosting

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,30 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
+        // Membuat Akun Admin
+        User::create([
+            'name' => 'Admin Arsip',
+            'email' => 'admin@bpk.go.id',
+            'password' => Hash::make('password_rahasia'), // Password login
+            'role' => 'admin',
         ]);
+
+        // Opsional: Membuat Akun Tamu (Jika perlu)
+        /*
+        User::create([
+            'name' => 'Tamu Kantor',
+            'email' => 'tamu@bpk.go.id',
+            'password' => Hash::make('tamu123'),
+            'role' => 'user',
+        ]);
+        */
     }
 }
